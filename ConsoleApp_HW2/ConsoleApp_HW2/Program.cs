538716aab2452a9a2fe68f3255a9947ec9da4941
Indra <indraberga@inbox.lv>
2021-09-08 22:42:40 +0300

    Username

---
+++ ConsoleApp_HW2/ConsoleApp_HW2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp_HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kā Tevi sauc?");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Sveiks, " + userName);
+        }
+    }
+}
