8faf42b696eb34d35e73955689d74baa9197a0c7
Indra <indraberga@inbox.lv>
2021-09-11 21:59:47 +0300

    Max+Min numbers

--- ConsoleApp_HW2/ConsoleApp_HW2/Program.cs
+++ ConsoleApp_HW2/ConsoleApp_HW2/Program.cs
@@ -30,7 +30,32 @@ namespace ConsoleApp_HW2
             Console.WriteLine(isAdult ? "tu esi pilngadīgs. " : "tu neeesi pilngadīgs. ");
 
             //------------------------------------------------------
+            Console.WriteLine("=======================================");
 
+            Console.WriteLine("Lūdzu, norādi 1. skaitli:");
+            string Number1 = Console.ReadLine();
+            int randomNr1 = int.Parse(Number1);
+
+            Console.WriteLine("Lūdzu, norādi 2. skaitli:");
+            string Number2 = Console.ReadLine();
+            int randomNr2 = int.Parse(Number2);
+
+            int maxResult = Math.Max(randomNr1, randomNr2);
+
+            Console.WriteLine("Lielākais no piedāvātajiem skaitļiem ir: " + maxResult);
+
+            //------------------------------------------------------
+            Console.WriteLine("Lūdzu, norādi 1. skaitli:");
+            string Number3 = Console.ReadLine();
+            int randomNr3 = int.Parse(Number3);
+
+            Console.WriteLine("Lūdzu, norādi 2. skaitli:");
+            string Number4 = Console.ReadLine();
+            int randomNr4 = int.Parse(Number4);
+
+            int minResult = Math.Min(randomNr3, randomNr4);
+
+            Console.WriteLine("Mazākais no piedāvātajiem skaitļiem ir: " + minResult);
 
 
         }
